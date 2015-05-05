56c0c547c3472167927af492e435b64f70eb276f
ypling <leoanrd.li.pub@gmail.com>
2015-05-05 09:22:31 -0700

    init

---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+/**
+ * Created by ypling on 5/4/15.
+ */
+'use strict';
+
+angular.module('ROIClientApp', ['ngRoute'])
+    .config(function ($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/planforward', {
+                templateUrl: './views/planforward.html',
+                controller:'forwardCtrl'
+            })
+            .when('/lookback', {
+                templateUrl: './views/lookback.html',
+                controller:'backCtrl'
+            })
+            .when('/mysenarios', {
+                templateUrl: './views/mysenarios.html',
+                controller:'scenariosCtrl'
+            })
+            .when('/compare', {
+                templateUrl: './views/compare.html',
+                controller:'compareCtrl'
+            })
+            .otherwise({
+                templateUrl: './views/dashboard.html',
+                controller:''
+            })
+    });
