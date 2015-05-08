7093928d35b255ecab5c164583417d49c91fbbb3
ypling <leoanrd.li.pub@gmail.com>
2015-05-08 14:32:26 -0700

    implemented mySenarios

--- scripts/main.js
+++ scripts/main.js
@@ -3,27 +3,50 @@
  */
 'use strict';
 
-angular.module('ROIClientApp', ['ngRoute'])
+var app = angular.module('ROIClientApp', ['ngRoute'])
     .config(function ($routeProvider, $locationProvider) {
         $routeProvider
             .when('/planforward', {
                 templateUrl: './views/planforward.html',
-                controller:'forwardCtrl'
+                controller: 'forwardCtrl'
             })
             .when('/lookback', {
                 templateUrl: './views/lookback.html',
-                controller:'backCtrl'
+                controller: 'backCtrl'
             })
             .when('/mysenarios', {
                 templateUrl: './views/mysenarios.html',
-                controller:'scenariosCtrl'
+                controller: 'scenariosCtrl'
             })
             .when('/compare', {
                 templateUrl: './views/compare.html',
-                controller:'compareCtrl'
+                controller: 'compareCtrl'
             })
             .otherwise({
                 templateUrl: './views/dashboard.html',
-                controller:''
+                controller: ''
             })
     });
+
+app.controller("scenariosCtrl", function ($scope) {
+        $scope.path=
+            [{title:"Scenarios",link:function(){}},
+             {title:"Save",link:function(){}}
+            ];
+        $scope.scenarios = [];
+        $scope.scenarios.push({
+            id: "id",
+            name: "name",
+            note: "note",
+            createDate: "createDate",
+            beginMonth: "beginMonth",
+            endMonth: "endMonth",
+            brand: "brand",
+            plannedSpend: "plannedSpend",
+            AM: "AM",
+            historyIncluded: "historyIncluded",
+            DataThrough: "DataThrough",
+            shared: "shared"
+        })
+    }
+);
